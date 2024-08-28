5b935d03bbf05aa228c90c767f2296cd72d5d575
vinileoml <rendrik120903@edu.unipar.br>
2024-08-28 20:32:02 -0300

    Atividade 2 Feita

--- src/ClassificacaoNotas.java
+++ src/ClassificacaoNotas.java
@@ -2,7 +2,22 @@ import java.util.Scanner;
 
 public class ClassificacaoNotas {
     public static void main(String[] args) {
-        Double nota = 2.0;
+        Double nota = 0.0;
         Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite uma nota: ");
+        nota = entrada.nextDouble();
+        if (nota >= 9.0) {
+            System.out.println("Excelente");
+
+        }
+        else if (nota >=7.0) {
+            System.out.println("Bom");
+        }
+        else if (nota >=5.0) {
+            System.out.println("Satisfatorio");
+        }
+        else if (nota <5.0) {
+            System.out.println("Insatisfatorio");
+        }
     }
 }
