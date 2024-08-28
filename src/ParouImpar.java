46bbf2bc84f087b57de2706ea1583383b6d3b4b6
vinileoml <rendrik120903@edu.unipar.br>
2024-08-28 20:35:02 -0300

    Atividade 3 Feita

---
+++ src/ParouImpar.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ParouImpar {
+    public static void main(String[] args) {
+        Integer numero = 0;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite um numero: ");
+        numero = entrada.nextInt();
+        if (numero % 2 == 0) {
+            System.out.println("Par");
+        }
+        else {
+            System.out.println("Impar");
+        }
+    }
+}
