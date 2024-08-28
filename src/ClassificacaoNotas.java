c813f562b14ad6966ecb203f6e40a5afddf53629
vinileoml <rendrik120903@edu.unipar.br>
2024-08-28 20:23:32 -0300

    Atividade 1 Feita

--- src/ClassificacaoNotas.java
+++ src/ClassificacaoNotas.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ClassificacaoNotas {
     public static void main(String[] args) {
-        Double nota = 1.0;
+        Double nota = 2.0;
         Scanner entrada = new Scanner(System.in);
     }
 }
