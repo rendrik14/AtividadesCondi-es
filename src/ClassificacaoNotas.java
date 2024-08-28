9aefb65bb1950fae2cbfd496b039d9c2b98757f9
vinileoml <viniciusleomoraisdelima@gmail.com>
2024-08-28 20:17:15 -0300

    Atividade 1 Feita

---
+++ src/ClassificacaoNotas.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class ClassificacaoNotas {
+    public static void main(String[] args) {
+        Double nota = 1.0;
+        Scanner entrada = new Scanner(System.in);
+    }
+}
