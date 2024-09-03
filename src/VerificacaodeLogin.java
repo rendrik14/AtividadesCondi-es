0ecfd77fac29e40ba27d8a2fe1ac5533fbbff4c2
vinileoml <rendrik120903@edu.unipar.br>
2024-09-02 21:13:59 -0300

    Atividade 4 completa

---
+++ src/VerificacaodeLogin.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class VerificacaodeLogin {
+
+    public static void main(String[] args) {
+
+
+        String login = "admin";
+
+        String password = "admin";
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Login: ");
+        String loginInformado = sc.nextLine();
+
+        System.out.print("Password: ");
+        String passwordInformada = sc.nextLine();
+
+        if (login.equals(loginInformado) && password.equals(passwordInformada)) {
+            System.out.println("Login com sucesso!");
+        } else {
+            System.out.println("Login ou Senha incorreta!");
+        }
+
+    }
+}
