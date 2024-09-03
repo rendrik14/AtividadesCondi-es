8c8d8944a5101599e9f1b4a2a94b6db39fc0627c
vinileoml <rendrik120903@edu.unipar.br>
2024-09-02 21:44:03 -0300

    Atividade 6 completa

---
+++ src/VerificacaoAprovacaoEscolar.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class VerificacaoAprovacaoEscolar {
+    public static void main(String[] args) {
+        double media=0.0;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite a media do aluno: ");
+        media = entrada.nextDouble();
+        if (media>=7.0 && media<=10.0) {
+            System.out.println("Aluno aprovado com sucesso!");
+        }
+        else if (media>=5.0 && media<7.0) {
+            System.out.println("Recuperação");
+        }
+        else if (media <5.0) {
+            System.out.println("Aluno reprovado");
+        }
+    }
+}
